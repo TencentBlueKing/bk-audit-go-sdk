72670f595c31d9200f34a6aebe6e5b48e2cea610
orenzhang <orenzhang@ovinc.cn>
2023-02-13 16:41:29 +0800

    minor: codecc

--- example/continues_export/main.go
+++ example/continues_export/main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	initRunParams()
-	fmt.Printf("Init Run Params Finished; TotalRuntime => %d; ExportEach => %d; SleepTime => %s\n", totalRunTime, exportEach, sleepTime)
+	fmt.Printf(
+		"Init Run Params Finished; TotalRuntime => %d; ExportEach => %d; SleepTime => %s\n",
+		totalRunTime,
+		exportEach,
+		sleepTime,
+	)
 	initLogFile()
 	fmt.Printf("Init Log File Finished; File Name => %s\n", logFileName)
 	initClient()
