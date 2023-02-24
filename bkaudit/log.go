fc80f7c2ac499bbd9c6e6587f3a10d7cecdcac36
orenzhang <orenzhang@ovinc.cn>
2023-02-24 11:32:57 +0800

    minor: export event with plain text

--- bkaudit/log.go
+++ bkaudit/log.go
@@ -2,17 +2,14 @@ package bkaudit
 
 import (
 	log "github.com/sirupsen/logrus"
-	"time"
 )
 
+type PlainTextFormatter struct{}
+
+func (f *PlainTextFormatter) Format(entry *log.Entry) (b []byte, err error) {
+	return append([]byte(entry.Message), '\n'), nil
+}
+
 func initLog() {
-	log.SetFormatter(&log.JSONFormatter{
-		TimestampFormat:   time.RFC3339,
-		DisableTimestamp:  false,
-		DisableHTMLEscape: true,
-		PrettyPrint:       false,
-		FieldMap: log.FieldMap{
-			log.FieldKeyMsg: "message",
-		},
-	})
+	log.SetFormatter(&PlainTextFormatter{})
 }
